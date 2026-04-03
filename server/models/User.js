8d165578c8c9cca58d9cf4426207ec3f0bd29ae4
Himanshu Choudhary <chaudharyhimanshu276@gmail.com>
2026-04-03 10:48:27 +0530

    .

--- server/models/User.js
+++ server/models/User.js
@@ -37,5 +37,5 @@ userSchema.pre('save', async function (next) {
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
-module.exports = mongoose.model('User', userSchema);
 
+module.exports = mongoose.model('User', userSchema);
